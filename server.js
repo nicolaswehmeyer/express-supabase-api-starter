b065a84455ca644815057dd93bd4b54eb9f04eca
Nicolas Wehmeyer <nicolas.wehmeyer@gmail.com>
2024-03-30 00:05:15 +0200

    Added Update functionalities

--- server.js
+++ server.js
@@ -60,6 +60,56 @@ app.post('/languages', async function (req, res) {
   }
 })
 
+app.get('/languages/:name', async function (req, res) {
+  const name = req.params.name
+
+  try {
+    const { data, error } = await supabase
+      .from('languages')
+      .select()
+      .ilike('name', name)
+    if (error) {
+      logger.error(error)
+      res.status(500).send(error.message)
+    } else {
+      res.status(200).json(data)
+    }
+  } catch (err) {
+    logger.error(err)
+    res.status(500).send(err.message)
+  }
+})
+
+app.put('/languages/:id', async function (req, res) {
+  const id = req.params.id
+  const name = req.body.name
+  const description = req.body.description
+  const stars = req.body.stars
+
+  try {
+    const { error } = await supabase
+      .from('languages')
+      .update({
+        name: name,
+        description: description,
+        stars: stars
+      })
+      .eq('id', id)
+
+    if (error) {
+      logger.error(error)
+      res.status(500).send(error.message)
+    } else {
+      res.status(200).json({
+        message: `${id} updated successfully`
+      })
+    }
+  } catch (err) {
+    logger.error(err)
+    res.status(500).send(err.message)
+  }
+})
+
 app.delete('/languages/:id', async function (req, res) {
   const id = req.params.id
 
@@ -76,26 +126,6 @@ app.delete('/languages/:id', async function (req, res) {
       res.status(200).json({
         message: `${id} removed successfully`
       })
-    }
-  } catch (err) {
-    logger.error(err)
-    res.status(500).send(err.message)
-  }
-})
-
-app.get('/languages/:name', async function (req, res) {
-  const name = req.params.name
-
-  try {
-    const { data, error } = await supabase
-      .from('languages')
-      .select()
-      .ilike('name', name)
-    if (error) {
-      logger.error(error)
-      res.status(500).send(error.message)
-    } else {
-      res.status(200).json(data)
     }
   } catch (err) {
     logger.error(err)
